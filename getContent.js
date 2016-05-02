943be47d370701677cc092f2bf7006822c4cca11
Thomas Burkart <tb085@hdm-stuttgart.de>
2016-05-02 10:50:34 +0200

    letzte Version

--- getContent.js
+++ getContent.js
@@ -28,27 +28,34 @@ for(var i = 0; i < twitterFeeds.length; i++) {
     // Twitter
     var twitterUid = twitterFeeds[i];
 
-    var params = {screen_name: twitterUid, count: 5, trim_user: true};
-    //client.get('statuses/user_timeline', params, function (error, tweets, response) {
-    //    if (!error) {
-    //        console.log(tweets);
-    //    }
-    //});
+    var params = {screen_name: twitterUid, count:10, trim_user: true};
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                var tweet = tweets[i];
+                console.log(tweet.text);
+            }
+        }
+    });
 
     // RSS Feed
     var rssLink = rssFeeds[i];
     console.log("start crawling: " + rssLink);
     var rssContent = crawlFeed(rssLink);
     var xmlContent = xml2js.parseString(rssContent, function (err, result) {
+        try {
         for (var x = 0; x < result.rss.channel.length; x++) {
             for (var y = 0; y < result.rss.channel[x].title.length; y++) {
                 console.log(result.rss.channel[x].title[y]);
             }
             for (var j = 0; j < result.rss.channel[x].item.length && j < 10; j++) {
                 var item = result.rss.channel[0].item[j];
-                console.log(item.title[0]);
-                console.log(item.description[0]);
+                console.log(item.title[0].trim());
+                console.log(item.description[0].trim());
             }
+        }
+        } catch(err) {
+            console.log(err);
         }
         console.log('Done');
     });
